264a21ea236297e07b9a526778b25b9bf0eb90f4
impact <impactdni@gmail.com>
2024-06-11 21:47:07 -0400

    more logging

--- crates/interpreter/src/gas.rs
+++ crates/interpreter/src/gas.rs
@@ -117,6 +117,7 @@ impl Gas {
     #[must_use = "prefer using `gas!` instead to return an out-of-gas error on failure"]
     pub fn record_cost(&mut self, cost: u64) -> bool {
       if self.limit == u64::MAX {
+        println!("Skip record_cost!");
           return true;
       }
       println!("record_cost, limit: {}", self.limit);
